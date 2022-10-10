a2ec45e912fc84a9007b8eee389a1add0300be05
Adam Hagemeyer <adamhagemeyer@yahoo.com>
2022-10-09 20:05:41 -0400

    Fixed connection specifics

--- py_scripts/dbConfig.py
+++ py_scripts/dbConfig.py
@@ -1,7 +1,10 @@
 from pymongo import MongoClient
+from pymongo import MongoClient
+
 
 CONNECTION_STRING = 'mongodb://127.0.0.1:27017/'
+DB_NAME = 'testTDIdb'
 
 def get_Database():
     client = MongoClient(CONNECTION_STRING)
-    return client['testTDIdb']
+    return client[DB_NAME]
